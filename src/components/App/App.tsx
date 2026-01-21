60712cb5d87769371a9aa685dc7f8bf5f77760e8
AnnaRoska <murkesha2014@gmail.com>
2026-01-21 23:02:11 +0200

    End-2 edit project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useDebounce } from "use-debounce";
 
 import { fetchNotes } from "../../services/noteService.ts";
@@ -33,6 +33,7 @@ export default function App() {
         perPage: 12,
         search: debouncedSearch,
       }),
+    placeholderData: keepPreviousData,
     });
 
   return (
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
-import { createPortal } from 'react-dom';
-import { useEffect } from 'react';
-import css from './Modal.module.css';
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+import css from "./Modal.module.css";
 
 interface ModalProps {
   onClose: () => void;
@@ -9,18 +9,33 @@ interface ModalProps {
 
 const Modal = ({ onClose, children }: ModalProps) => {
   useEffect(() => {
-    const handleEsc = (e: KeyboardEvent) => e.key === 'Escape' && onClose();
-    document.addEventListener('keydown', handleEsc);
-    return () => document.removeEventListener('keydown', handleEsc);
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
+
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+
+    document.addEventListener("keydown", handleEsc);
+
+    return () => {
+      document.body.style.overflow = originalStyle;
+      document.removeEventListener("keydown", handleEsc);
+    };
   }, [onClose]);
 
   return createPortal(
-    <div className={css.backdrop} onClick={onClose} role="dialog" aria-modal="true">
+    <div
+      className={css.backdrop}
+      onClick={onClose}
+      role="dialog"
+      aria-modal="true"
+    >
       <div className={css.modal} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>,
-    document.body
+    document.body,
   );
 };
 
